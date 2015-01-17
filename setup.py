d272f0c91ba0756a80bdd8eaabc48af22cba3f76
Suchakra Sharma <suchakrapani.sharma@polymtl.ca>
2015-01-16 23:29:30 -0500

    Add more requires

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-        'askbot', 'django-celery', 'importlib', 'amqp'
+        'askbot', 'django-celery', 'importlib', 'amqp', 'oauth2', 'django-appconf'
     ],
 )
